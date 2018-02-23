d664d1b28a7ea00203cd3ce1a518099ffc91831d
haoliangyu <njkon@foxmail.com>
2018-02-22 23:10:15 -0500

    search input box working

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,12 +1,28 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, AfterViewInit } from "@angular/core";
 
 @Component({
   selector: "app-main-page",
   templateUrl: "./main-page.component.html",
   styleUrls: ["./main-page.component.scss"]
 })
-export class MainPageComponent implements OnInit {
+export class MainPageComponent implements AfterViewInit {
   constructor() {}
 
-  ngOnInit() {}
+  ngAfterViewInit() {
+    /**
+     * The pure CSS solution doesn"t seem to work and it has to be done in TS.
+     */
+
+    const placeholder = document.getElementsByClassName(
+      "mat-input-placeholder"
+    )[0] as HTMLElement;
+    placeholder.style.color = "white";
+    placeholder.style.textAlign = "center";
+
+    const underline = document.getElementsByClassName(
+      "mat-input-ripple"
+    )[0] as HTMLElement;
+    underline.style.visibility = "visible";
+    underline.style.backgroundColor = "white";
+  }
 }
