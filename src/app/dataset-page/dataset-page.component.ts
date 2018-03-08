13558008eac46cb5d71da23fe1c78b0f1e8477d1
haoliangyu <njkon@foxmail.com>
2018-03-08 00:18:35 -0500

    fix a lint error

--- src/app/dataset-page/dataset-page.component.ts
+++ src/app/dataset-page/dataset-page.component.ts
@@ -46,7 +46,9 @@ export class DatasetPageComponent implements OnInit {
               L.tileLayer(
                 "http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png",
                 {
-                  attribution: `&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, Tiles courtesy of <a href="http://hot.openstreetmap.org/" target="_blank">Humanitarian OpenStreetMap Team</a>`
+                  attribution:
+                    `&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>` +
+                    `, Tiles courtesy of <a href="http://hot.openstreetmap.org/" target="_blank">Humanitarian OpenStreetMap Team</a>`
                 }
               ).addTo(this.map);
 
